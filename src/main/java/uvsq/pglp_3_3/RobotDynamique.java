44fbf44449581549014f0e49fb6e60bc5b4e315b
ismail <benammarismail31@gmail.com>
2020-03-30 15:48:40 +0200

    class robot dynamique

---
+++ src/main/java/uvsq/pglp_3_3/RobotDynamique.java
@@ -0,0 +1,9 @@
+package uvsq.pglp_3_3;
+
+public class RobotDynamique extends RobotStatique{
+	
+	String position;
+
+	public void avance() { }
+
+}
---
+++ src/main/java/uvsq/pglp_3_3/RobotStatique.java
@@ -0,0 +1,9 @@
+package uvsq.pglp_3_3;
+
+public abstract class RobotStatique{
+
+	String direction;
+
+	public void tourne() { }
+
+}
